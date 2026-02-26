5638c2a6f8f76c678024f37e5045118350ae649e
Sean-personal <xpzeng001@gmail.com>
2026-02-27 00:46:33 +0800

    refactor: replace Alibaba OSS with Tencent COS in ossService

--- server/services/ossService.js
+++ server/services/ossService.js
@@ -1,11 +1,34 @@
 const path = require('path');
 const crypto = require('crypto');
-const getOssClient = require('../config/oss');
+const COS = require('cos-nodejs-sdk-v5');
 
+const getCosClient = () => new COS({
+  SecretId: process.env.TENCENT_SECRET_ID,
+  SecretKey: process.env.TENCENT_SECRET_KEY
+});
+
+/**
+ * 上传文件到腾讯 COS
+ * @param {Express.Multer.File} file  multer 内存存储的文件对象
+ * @returns {Promise<string>}  COS 图片公网 URL
+ */
 exports.upload = async (file) => {
-  const client = getOssClient();
+  const cos = getCosClient();
   const ext = path.extname(file.originalname) || '.jpg';
   const key = `neighborhub/${Date.now()}_${crypto.randomBytes(4).toString('hex')}${ext}`;
-  const result = await client.put(key, file.buffer);
-  return result.url;
+  const bucket = process.env.COS_BUCKET;
+  const region = process.env.COS_REGION;
+
+  return new Promise((resolve, reject) => {
+    cos.putObject({
+      Bucket: bucket,
+      Region: region,
+      Key: key,
+      Body: file.buffer,
+      ContentLength: file.size
+    }, (err) => {
+      if (err) return reject(err);
+      resolve(`https://${bucket}.cos.${region}.myqcloud.com/${key}`);
+    });
+  });
 };
